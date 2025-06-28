d862f72c37c41c4c58cd2cd6f6454c56fa5e758f
11051 <1105101864@qq.com>
2025-06-29 02:04:25 +0800

    删除无效代码警告

--- src/renderer/src/utils/ipcRenderer.ts
+++ src/renderer/src/utils/ipcRenderer.ts
@@ -1,9 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const send = (code: string, ...args: any[]): void => {
   window.electron.ipcRenderer.send('common-on', code, ...args)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const invoke = (code: string, ...args: any[]): Promise<any> => {
   return window.electron.ipcRenderer.invoke('common-handle', code, ...args)
 }
